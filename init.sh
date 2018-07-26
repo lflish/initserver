ab4f7bc25d3df050ec8281fee16c489e22977d76
yu <68813175@qq.com>
2018-07-26 01:35:48 -0400

    up

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 #初始化脚本
 sh shell/yum.sh
+
+mv shell/ss.sh ~/
+
+#加入防火墙
+iptables -A IN_public_allow -p tcp -m tcp --dport 8086 -m conntrack --ctstate NEW -j ACCEPT
--- shell/yum.sh
+++ shell/yum.sh
@@ -1,1 +1,5 @@
 yum -y install vim gcc net-tools git lrzsz unzip wget
+
+#安装梯子
+yum -y install python-setuptools && easy_install pip
+pip install shadowsocks
