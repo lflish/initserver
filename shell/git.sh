95dc6a0ba2fa87763d33d1bd19bf7b8be4fff341
lflish <688613175@qq.com>
2018-07-28 21:52:51 +0800

    up git

---
+++ shell/git.sh
@@ -0,0 +1,3 @@
+git config --global user.email "688613175@qq.com"
+git config --global user.name "lflish"
+
--- shell/yum.sh
+++ shell/yum.sh
@@ -3,3 +3,6 @@ yum -y install vim gcc net-tools git lrzsz unzip wget
 #安装梯子
 yum -y install python-setuptools && easy_install pip
 pip install shadowsocks
+
+#linux客户端使用
+sslocal -c /etc/ss.json
