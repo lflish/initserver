8851a3cbe7467ac916984acded1b81c6340040a2
lflish <688613175@qq.com>
2018-09-06 01:14:10 -0400

    up check

--- shell/file_check.py
+++ shell/file_check.py
@@ -20,7 +20,9 @@ for ssh_file in sys.argv[1:]:
 				total_map[match[0]] += 1;
 			else:
 				total_map[match[0]] = 1;
-	print "\033[1;31;40m%s\033[0m"%ssh_file
-	print ssh_map;
-print "\n\033[1;31;40mtotal list\033[0m"
-print total_map;
+	#print "\033[1;31;40m%s\033[0m"%ssh_file
+	#print ssh_map;
+print "\033[1;31;40mtotal list\033[0m"
+for key in total_map:
+	if total_map[key] > 5:
+		print(key, '=>', total_map[key])
