4b57c92935d919385d3aaace4d023649f9dabfe0
huangxiangyu <huangxiangyu@chiansec.com>
2022-03-17 23:47:22 -0700

    更新生成git脚本

--- shell/git.sh
+++ shell/git.sh
@@ -1,8 +1,9 @@
-git config --global user.email "688613175@qq.com"
+git config --global user.email "hxy.gold@gmail.com"
 git config --global user.name "lflish"
 
 #记录密码
 git config --global credential.helper store
 
 #生成公钥
-ssh-keygen
+ssh-keygen -t ed25519 -C "hxy.gold@gmail.com"
+
