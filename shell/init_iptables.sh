f92987a81595f2a1c234ed6561b381cdd9594051
lflish <688613175@qq.com>
2022-10-11 17:45:22 +0800

    add iptables

--- shell/init_iptables.sh
+++ shell/init_iptables.sh
@@ -15,6 +15,8 @@ iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 #设置nginx端口放行
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 
+iptables -A  INPUT -s 192.168.100.100 -j ACCEPT
+
 #允许合法链接并丢弃非法连接
 iptables -I INPUT -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT 
 iptables -I INPUT -m state --state INVALID -j DROP
